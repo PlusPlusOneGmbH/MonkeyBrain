91e5bc1a3f9d20b1ac732e0caed2c20705cc47be
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-07-30 11:27:02 +0200

    Added loglevel env variable and some non-functional refactoring.,

--- src/touchlaunch/__init__.py
+++ src/touchlaunch/__init__.py
@@ -1,17 +1,19 @@
+from typing import Literal
+from enum import Enum
+
 import subprocess
-from dotenv import load_dotenv
 from os import environ, listdir
+
+from dotenv import load_dotenv
+
 from pathlib import Path
 from tomllib import load as loadToml
 
 import logging
-
 logger = logging.getLogger()
-logging.basicConfig(level=logging.INFO)
+log_level = getattr(logging, environ.get("TOUCHLAUNCH_LOGLEVEL", "INFO"), None) or logging.INFO
+logging.basicConfig(level=log_level)
 
-from typing import Literal
-
-from enum import Enum
 
 
 
@@ -30,29 +32,31 @@ def search_touchdesigner_folder(mode:SearchMode) -> Path:
     td_folder = []
     
     for search_path in [search_path for search_path in td_search_paths if search_path and Path(search_path).is_dir()]:
-        logger.info( f"Searching in {search_path}" )
+        
+        logger.debug( f"Searching in {search_path}" )
         for child in listdir( search_path ):
-            logger.info( f"checking {child}")
+            logger.debug( f"checking {child}")
             if Path(search_path, child).is_file(): 
-                logger.info("Is a file and not a dir.")
+                logger.debug("Is a file and not a dir.")
                 continue
             if not child.startswith(("touchdesigner", "TouchDesigner")): 
-                logger.info("Does not start with TouchDesigner, so not a valid install folder.")
+                logger.debug("Does not start with TouchDesigner, so not a valid install folder.")
                 continue
             if len( split_elements := child.split(".")) != 3: 
-                logger.info("Name needs to follow TouchDesigner.Version.Build")
+                logger.debug("Name needs to follow TouchDesigner.Version.Build")
                 continue
             version, build = split_elements[1], split_elements[2]
             td_folder.append(
                 ( version, build, f"{version}.{build}", Path( search_path, child ) )
             )
-    logger.info(f"Found the follwowing potential folder, {td_folder}")
+    logger.debug(f"Found the follwowing potential folder, {td_folder}")
     if not td_folder: raise Exception("Could not find any valid TouchDesigner installfolder. Make sure the correct version is installed.")
 
     if mode == SearchMode.STRICT.value:
         for element in td_folder:
-            logger.info(f"Checking required {td_version} against {element[2]}")
+            logger.debug(f"Checking required {td_version} against {element[2]}")
             if td_version == element[2]: return element[3]
+            logger.error(f"Could not find path for {td_version} in strict mode. Install specific version or change mode.")
             raise Exception(f"Could not find path for {td_version} in strict mode. Install specific version or change mode.")
     
     required_version, reuquired_build = td_version.split(".")
@@ -87,20 +91,25 @@ def search_touchdesigner_folder(mode:SearchMode) -> Path:
              reverse = True
         )[0][3]
     
-    raise Exception("Could not find a fitting installation to satisify {td_version} in {mode} mode.")
+    logger.error(f"Could not find a fitting installation to satisify {td_version} in {mode} mode.")
+    raise Exception(f"Could not find a fitting installation to satisify {td_version} in {mode} mode.")
 
 def launch(backend:Literal["TouchDesigner", "TouchPlayer"]):
     with Path("pyproject.toml").open("rb") as project_file:
         projectData = loadToml( project_file )
 
 
-    executeableName = f"{backend}.exe" # Sorry mac lol.
-    tool_config = projectData.get("tool", {}).get("touchdesigner", {})
-    project_file = tool_config.get("projectfile", "Project.toe")
-    search_mode = tool_config.get("enforce-version", "latest-build")
-    tdFolder = search_touchdesigner_folder(search_mode)
-    logger.info(f"Found installation {tdFolder}")
-    load_dotenv()
+    executeableName     = f"{backend}.exe" # Sorry mac lol.
+    tool_config         = projectData.get("tool", {}).get("touchdesigner", {})
+    project_file        = tool_config.get("projectfile", "Project.toe")
+    search_mode         = tool_config.get("enforce-version", "latest-build")
+    tdFolder            = search_touchdesigner_folder(search_mode)
+
+    logger.info(f"Found installation {tdFolder}.")
+
+    envLoaded = load_dotenv()
+    if envLoaded: logger.info("Loaded .env file.")
+
     tdExecuteable = Path(tdFolder, "bin", executeableName)
     logger.info(f"Executing {tdExecuteable} with {project_file}")
     tdProcess = subprocess.Popen([str(tdExecuteable),  project_file])
@@ -108,7 +117,6 @@ def launch(backend:Literal["TouchDesigner", "TouchPlayer"]):
    
 
 def designer():
-    
     launch("TouchDesigner")
 
 def editor():
